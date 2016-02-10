dc43862a88e24e324342573e1cc98dd6bed383e0
devkake <sofujikake.dev@gmail.com>
2016-02-10 23:59:27 +0100

    lorem ipsum

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
+void lorem_ipsum();
+
 int main() {
   cout << "Hello, master merged Branch 2!" << endl;
   return 0;
+}
+
+void lorem_ipsum() {
+	cout << "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." << endl;
 }
