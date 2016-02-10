623e4ce0890ed8148472723ab1fb720bb3a6a990
flm8620 <flm8620@gmail.com>
2016-02-11 00:04:41 +0100

    readfile

--- main.cpp
+++ main.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cassert>
+
+#ifndef srcPath
+#define srcPath "."
+#endif
+
+
 using namespace std;
+string inputFile = srcPath "/logo.in";
+void readFile(string fileName, vector<bool> &wall, int &n, int &m) {
+  ifstream input(fileName);
+  input >> n >> m;
+  wall.resize(n * m);
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      char c;
+      input >> c;
+      if (c == '.') {
+        wall[i * m + j] = false;
+      } else {
+        wall[i * m + j] = true;
+      }
+    }
+    char c;
+    input >> c;//delete '\n'
+    //assert(c == '\n');
+  }
+}
 int main() {
-  cout << "Hello, Branch 2, 2nd commit!" << endl;
+  int n, m;
+  vector<bool> wall;
+  readFile(inputFile,wall,n,m);
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      if (wall[i * m + j])
+        cout << '#';
+      else
+        cout << '.';
+    }
+    cout << endl;
+
+  }
   return 0;
 }
