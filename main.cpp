b48db966810ea4417c0be84f3fb5ab77a95e6595
devkake <sofujikake.dev@gmail.com>
2016-02-11 10:08:21 +0100

    test for proxy

--- main.cpp
+++ main.cpp
@@ -8,42 +8,43 @@
 #define srcPath "."
 #endif
 
-
 using namespace std;
 string inputFile = srcPath "/logo.in";
 void readFile(string fileName, vector<bool> &wall, int &n, int &m) {
-  ifstream input(fileName);
-  input >> n >> m;
-  wall.resize(n * m);
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      char c;
-      input >> c;
-      if (c == '.') {
-        wall[i * m + j] = false;
-      } else {
-        wall[i * m + j] = true;
-      }
-    }
-    //char c;
-    //input >> c;//delete '\n'
-    //cout<<c<<endl;
-    //assert(c == '\n');
-  }
+	ifstream input(fileName);
+	input >> n >> m;
+	wall.resize(n * m);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			char c;
+			input >> c;
+			if (c == '.') {
+				wall[i * m + j] = false;
+			} else {
+				wall[i * m + j] = true;
+			}
+		}
+		//char c;
+		//input >> c;//delete '\n'
+		//cout<<c<<endl;
+		//assert(c == '\n');
+	}
 }
-int main() {
-  int n, m;
-  vector<bool> wall;
-  readFile(inputFile,wall,n,m);
 
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      if (wall[i * m + j])
-        cout << '#';
-      else
-        cout << '.';
-    }
-    cout << endl;
-  }
-  return 0;
+int main() {
+	cout << "test for proxy" << endl;
+	int n, m;
+	vector<bool> wall;
+	readFile(inputFile, wall, n, m);
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (wall[i * m + j])
+				cout << '#';
+			else
+				cout << '.';
+		}
+		cout << endl;
+	}
+	return 0;
 }
