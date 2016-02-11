093a316d0fa8c290b18918aa5070b4f22023e0ae
Dylan <dylan.dronnier@laposte.net>
2016-02-11 20:19:46 +0100

    ok

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <chrono>
+#include <random>
+#include <cassert>
+
+struct 
+
+//void sortWare(vector<int> ware) {
+    
+
+
+map<int, Pai
